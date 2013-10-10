87970783da99e3d6a3ddf1996554ac750c863d6d
aku <aku>
2013-10-10 05:10:53 +0000

    Split watch state database initialization into separate command, and created finalization command.

--- build.tcl
+++ build.tcl
@@ -9,11 +9,13 @@ kettle tclapp bin/setup-export
 kettle tclapp bin/setup-import
 #
 kettle tclapp bin/watch-add
-kettle tclapp bin/watch-destroy
-kettle tclapp bin/watch-do
-kettle tclapp bin/watch-list
-kettle tclapp bin/watch-remove
-kettle tclapp bin/watch-setup
 kettle tclapp bin/watch-config-get
 kettle tclapp bin/watch-config-set
 kettle tclapp bin/watch-config-unset
+kettle tclapp bin/watch-destroy
+kettle tclapp bin/watch-do
+kettle tclapp bin/watch-final
+kettle tclapp bin/watch-init
+kettle tclapp bin/watch-list
+kettle tclapp bin/watch-remove
+kettle tclapp bin/watch-setup
