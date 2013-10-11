723c26fc2aa82909143c8f136c2d77956814ba85
aku <aku>
2013-10-11 04:03:36 +0000

    Added helpers to see the raw timeline RSS, and specific artifacts.

--- build.tcl
+++ build.tcl
@@ -14,9 +14,12 @@ kettle tclapp bin/watch-config-set
 kettle tclapp bin/watch-config-unset
 kettle tclapp bin/watch-destroy
 kettle tclapp bin/watch-do
+kettle tclapp bin/watch-dump
 kettle tclapp bin/watch-expire
 kettle tclapp bin/watch-final
 kettle tclapp bin/watch-init
 kettle tclapp bin/watch-list
 kettle tclapp bin/watch-remove
+kettle tclapp bin/watch-rss
 kettle tclapp bin/watch-setup
+kettle tclapp bin/watch-unsee
