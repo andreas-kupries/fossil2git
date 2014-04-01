19d5949f42f3e1ce2770039b574a596c39e862f6
aku <aku>
2014-04-01 07:19:55 +0000

    Extended to retrieve timeline event information.

--- lib/seen.tcl
+++ lib/seen.tcl
@@ -19,7 +19,8 @@ package require fx::fossil
 namespace eval ::fx::seen {
     namespace export forall-pending \
 	mark-notified mark-notified-all \
-	mark-pending mark-pending-all
+	mark-pending mark-pending-all \
+	get-event
     namespace ensemble create
 
     namespace import ::fx::fossil
@@ -31,6 +32,25 @@ namespace eval ::fx {
 }
 
 # # ## ### ##### ######## ############# ######################
+
+proc ::fx::seen::get-event {uuid} {
+    fossil repository eval {
+	SELECT event.type  AS type,
+	       event.objid AS id,
+	       blob.uuid   AS uuid,
+	       coalesce (event.ecomment, event.comment) AS comment
+	FROM  event, blob
+	WHERE blob.uuid = :uuid
+	AND   event.objid = blob.rid
+    } {
+	return [dict create type $type id $id uuid $uuid comment $comment]
+    }
+    # NOTE: We will be shelling out to fossil to get the artifact
+    # contents. This way we avoid having to implement the entire
+    # decompression (delta, inflate) ourselves. A 'libfossil' with a
+    # proper C API would make this easier.
+    return
+}
 
 proc ::fx::seen::forall-pending {tv iv uv cv script} {
     Init
