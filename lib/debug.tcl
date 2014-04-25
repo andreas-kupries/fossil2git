665fd554dca8ac36ea1e0162b0c71b4cbba18fb0
aku <aku>
2014-04-25 03:42:32 +0000

    New command "debug levels", to inspect the narrator.

---
+++ lib/debug.tcl
@@ -0,0 +1,64 @@
+## -*- tcl -*-
+# # ## ### ##### ######## #############
+
+package require Tcl 8.5
+package require debug
+
+package require fx::table
+
+# # ## ### ##### ######## #############
+
+namespace eval ::fx {
+    namespace export debug
+    namespace ensemble create
+}
+namespace eval ::fx::debug {
+    namespace export levels
+    namespace ensemble create
+
+    namespace import ::fx::table::do
+    rename do table
+}
+
+# # ## ### ##### ######## #############
+
+proc ::fx::debug::levels {config} {
+    # First ensure that all possible fx packages are loaded, so that
+    # all possible debug levels are declared and known.
+
+    package require fx::config
+    package require fx::enum
+    package require fx::fossil
+    package require fx::mailer
+    package require fx::mailgen
+    package require fx::manifest
+    package require fx::mgr::config
+    package require fx::note
+    package require fx::report
+    package require fx::seen
+    package require fx::table
+    package require fx::tty
+    package require fx::user
+    package require fx::user
+    package require fx::validate::enum
+    package require fx::validate::enum-item
+    package require fx::validate::event-type
+    package require fx::validate::mail-address
+    package require fx::validate::mail-config
+    package require fx::validate::not-enum
+    package require fx::validate::not-enum-item
+    package require fx::validate::setting
+    package require fx::validate::ticket-field
+    package require fx::validate::user
+    package require fx::validate::uuid
+
+    [table t {Level} {
+	foreach level [lsort -dict [debug names]]  {
+	    $t add $level
+	}
+    }] show
+    return
+}
+
+# # ## ### ##### ######## #############
+package provide fx::debug 0
