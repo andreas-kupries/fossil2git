9dbb5874df019d43be6d9d85b2b053936deab348
andreask <andreask>
2014-05-14 21:03:11 +0000

    Installation - Reactivate install of "cron_lock" helper, and fully drop the old set of watch-* apps.

--- build.tcl
+++ build.tcl
@@ -7,7 +7,7 @@ exec kettle -f "$0" "${1+$@}"
 
 # New general tool (Fossil eXtended)
 kettle tclapp fx
-#kettle tclapp cron_lock ;# Actually a sh app.
+kettle tclapp cron_lock
 kettle tcl
 
 ## One-way mirroring of fossil repositories to git
@@ -15,20 +15,3 @@ kettle tcl
 #kettle tclapp bin/list
 #kettle tclapp bin/setup-export
 #kettle tclapp bin/setup-import
-##
-## Tracking ticket changes in fossil repositories
-#kettle tclapp bin/watch-add
-#kettle tclapp bin/watch-config-get
-#kettle tclapp bin/watch-config-set
-#kettle tclapp bin/watch-config-unset
-#kettle tclapp bin/watch-destroy
-#kettle tclapp bin/watch-do
-#kettle tclapp bin/watch-dump
-#kettle tclapp bin/watch-expire
-#kettle tclapp bin/watch-final
-#kettle tclapp bin/watch-init
-#kettle tclapp bin/watch-list
-#kettle tclapp bin/watch-remove
-#kettle tclapp bin/watch-rss
-#kettle tclapp bin/watch-setup
-#kettle tclapp bin/watch-unsee
