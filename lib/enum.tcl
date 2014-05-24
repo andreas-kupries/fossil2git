ea24ba65fadf9b69c703183e1ad7bc9021135958
andreask <andreask>
2014-05-24 00:09:24 +0000

    fx::enum - Move opening of channel for export, and actually open it.

--- lib/enum.tcl
+++ lib/enum.tcl
@@ -49,8 +49,7 @@ namespace eval ::fx::enum {
     namespace import ::fx::mgr::enum
     rename enum mgr
 
-    # After the manager has been handled,
-    # avoid conflict.
+    # After the manager has been handled, avoid conflict.
     namespace import ::fx::validate::enum
 }
 
@@ -204,7 +203,6 @@ proc ::fx::enum::export {config} {
     } else {
 	set enums [fossil fx-enums]
     }
-    set chan [$config @output]
 
     lappend data "\# fx enumeration export @ [clock format [clock seconds]]"
     foreach enum $enums {
@@ -215,7 +213,8 @@ proc ::fx::enum::export {config} {
 	lappend data end
     }
 
-    puts $chan [join $data \n]
+    set    chan [open [$config @output] w]
+    puts  $chan [join $data \n]
     close $chan
     return
 }
